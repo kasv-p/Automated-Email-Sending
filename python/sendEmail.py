894467284eec722a2c4c6fd53900989f2afbc88d
kasv-p <84275697+kasv-p@users.noreply.github.com>
2022-12-23 11:55:19 +0530

    Update sendEmail.py

--- python/sendEmail.py
+++ python/sendEmail.py
@@ -1,13 +1,13 @@
-# https://myaccount.google.com/apppasswords create that password for email and place it in place of password
+
 
 # if same subject then all goes to same thread only
 
 import smtplib
 toaddr = ''
 cc = []
-bcc = ['kvlsvani2468@gmail.com','kasvtaxes@gmail.com']
-fromaddr = 'ksvd1234@gmail.com'
-password='grpjaxxbfuvysned'
+bcc = ['bccaddr1','bccaddr2']
+fromaddr = 'fromaddr'
+password='apppassword'
 message_subject = "hi"
 message_text = "good good evening"
 message = "From: %s\r\n" % fromaddr + "To: %s\r\n" % toaddr + "CC: %s\r\n" % ",".join(cc) + "Subject: %s\r\n" % message_subject + "\r\n" + message_text
