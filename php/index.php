14036bd403a86d9432d47fdb36a34e3357b2b1e5
kasv-p <84275697+kasv-p@users.noreply.github.com>
2022-12-23 22:19:12 +0530

    Update index.php

--- php/index.php
+++ php/index.php
@@ -2,8 +2,5 @@
 
 ?>
 <form id="contact" action='send_mail.php' method="post">
-name <input name='name' type="text"><br>
-email <input name='email' type="text"><br>
-tel <input name='tel' type="text"><br>
 <button type="submit" name='submit'>submit</button>
 </form>
